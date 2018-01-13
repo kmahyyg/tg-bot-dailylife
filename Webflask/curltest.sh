b3ec39cf38ee1cd9042e6ff12a420eb490738ef5
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2018-01-13 23:38:48 +0800

    SQLite3 - Add, Modify, Delete, Checkout DONE!
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- Webflask/curltest.sh
+++ Webflask/curltest.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -H 'Content-Type: application/json' -X POST -d '{"name":"yyg2","tel":"111211"}' http://127.0.0.1:5000
+#curl -H 'Content-Type: application/json' -X POST -d '{"name":"yyg2","tel":"111211"}' http://127.0.0.1:5000
+curl -X DELETE 'http://127.0.0.1:5000/api/checktel?name=yyg3'
 exit 0
