ffba2c561606b5e674b94943dba371a86d298dbd
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2018-01-18 01:44:33 +0800

    pkgbot ReSTful API finished.
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>
    
    pkgbot debug sqlalchemy
    
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- Webflask/pkgbot_auth.py
+++ Webflask/pkgbot_auth.py
@@ -11,20 +11,32 @@ from aliyun_exp import packagereq
 app = Flask(__name__)
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/sqlalc?charset=utf8mb4'
 db = SQLAlchemy(app)
 
 
 class Package(db.Model):
     __tablename__ = 'pkglist'
-    pkgid = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    pkgid = db.Column(db.VARCHAR(32), primary_key=True, nullable=False, unique=True)
     pkgstatus = db.Column(db.Integer, nullable=False)
     pkgtrack = db.Column(db.VARCHAR(255))
-    tguserid = db.Column(db.VARCHAR(255))
+    tguserid = db.Column(db.VARCHAR(32))
 
+    def __init__(self, pkgid, pkgstatus, pkgtrack, tguserid):
+        self.pkgid = pkgid
+        self.pkgstatus = pkgstatus
+        self.pkgtrack = pkgtrack
+        self.tguserid = tguserid
+
+    def __repr__(self):
+        result = {'pkgid': self.pkgid, 'pkgstatus': self.pkgstatus, 'pkgtrack': self.pkgtrack,
+                  'tguserid': self.tguserid}
+        return json.dumps(result)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
+    db.session.commit()
     db.session.remove()
 
 
@@ -38,8 +50,9 @@ def callpkgapi():
         msg = json.loads(respA)
         return respA
     companyrep = respA['result']['type']
+    pkgtrack = respA['result']['list'][0]
     pkgstat = respA['result']['deliverystatus']
-    package = Package(pkgid=expno, pkgstatus=pkgstat, pkgtrack=respA['result']['list'][0], tguserid=userid)
+    package = Package(pkgid=expno, pkgstatus=pkgstat, pkgtrack=pkgtrack, tguserid=userid)
     if (pkgstat is not 3):
         if (Package.query.filter_by(pkgid=expno).first() is not None):
             package.pkgtrack = respA['result']['list'][0]
@@ -47,7 +60,7 @@ def callpkgapi():
         else:
             db.session.add(package)
             db.session.commit()
-        return jsonify({'id': expno, 'stat': pkgstat, 'track': package.pkgtrack})
+        return jsonify({'id': expno, 'stat': pkgstat, 'track': pkgtrack})
     else:
         db.session.delete(package)
         return jsonify({'code': 400, 'bmsg': 'The package is already received by user.'})
@@ -57,8 +70,9 @@ def callpkgapi():
 def checkbyuser():
     userid = request.json.get('tgid')
     usersql = Package.query.filter_by(tguserid=userid).first()
-    while (usersql is not None):
-        return jsonify()
+    # if (usersql is not None):
+    #
+    # else:
+    return jsonify({'code': 404, 'bmsg': 'not found'})
 
-
-app.run(debug=True)
+# app.run(debug=True)
