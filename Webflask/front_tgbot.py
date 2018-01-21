2677ebff755705c8457c34af34e875914ebba4e7
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2018-01-21 21:44:16 +0800

    update frontend
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

---
+++ Webflask/front_tgbot.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+
+import json, requests, logging
+from apikey import tgbottoken
+from telegram.ext import Updater
+from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError)
+
+# Bot define using class telegram.Bot
+# reqarg = telegram.utils.request.Request(proxy_url='socks5://127.0.0.1:9099')
+# bot = telegram.Bot(token=tgbottoken,request=reqarg)
+
+# Test Environment
+updater = Updater(token=tgbottoken, request_kwargs={
+    'proxy_url': 'socks5://127.0.0.1:9099/'
+})
+
+# Production Environment
+# updater = Updater(token=tgbottoken)
+
+dispatcher = updater.dispatcher
+
+# Error Handler and Log
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG, filename='tgpkgbot.log')
+
+
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except Unauthorized:
+        print('unauthorized')
+        return json.dumps({'code': 401, 'bmsg': 'unauthorized'})
+        # remove update.message.chat_id from conversation list
+    except BadRequest:
+        print('BadRequest')
+        return json.dumps({'code': 400, 'bmsg': 'bad request'})
+        # handle malformed requests - read more below!
+    except TimedOut:
+        print('timed out')
+        return json.dumps({'code': 408, 'bmsg': 'request timed out'})
+        # handle slow connection problems
+    except NetworkError:
+        print('network error')
+        return json.dumps({'code': 502, 'bmsg': 'network error'})
+        # handle other connection problems
+    except ChatMigrated as e:
+        pass
+        # the chat_id of a group has changed, use e.new_chat_id instead
+    except TelegramError:
+        return json.dumps({'code': 500, 'bmsg': 'server error'})
+        # handle all other telegram related errors
+
+
+dispatcher.add_error_handler(error_callback)
+
+# Send message with commands
+# works with requests on flask.
