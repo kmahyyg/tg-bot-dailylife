eae2f71914df0c47d0dab7dac9d981c58e255784
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2018-01-22 15:52:56 +0800

    add mime judge and update requirements.txtP
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- Webflask/mailgun_bn.py
+++ Webflask/mailgun_bn.py
@@ -2,3 +2,11 @@
 # -*- encoding: utf-8 -*-
 
 from apikey import mgcode
+import requests
+from winmagic import magic
+
+
+def getfilemime(fileloc):
+    mime = magic.Magic(mime=True)
+    mimeget = mime.from_file(fileloc)
+    return mimeget
