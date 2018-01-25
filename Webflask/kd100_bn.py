e13699dd56ae8365222eca9d6d6cff98c2c4fbdd
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2018-01-25 15:48:46 +0800

    KuaiDi100 3rd-party API introduced.
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- Webflask/kd100_bn.py
+++ Webflask/kd100_bn.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 #-*- encoding: utf-8 -*-
 
-# 3rd
+# 3rd party API : KD100
+# http://www.kuaidi100.com/autonumber/autoComNum?resultv2=1&text= <EXP CODE>  [POST]
+# http://www.kuaidi100.com/query?type= <COMPANY CODE> &postid= <EXP CODE>  [GET]
+
+# POST : resp['auto'][0]['comCode']
+# raise IndexError (None Array)
+
+# GET: pkgresp['data'][0]['context']
+# check if pkgresp['status'] == 200, else is error
+
+# kd100_company.json http://www.kuaidi100.com/js/share/company.js
